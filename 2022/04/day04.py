f5d66b31ee7d23b68a8fefb30957f088f0da5de4
Jon <stu229763@uni-kiel.de>
2022-12-04 14:37:50 +0100

    day04: readabilty

--- 2022/04/day04.py
+++ 2022/04/day04.py
@@ -1,15 +1,14 @@
 with open("input.txt") as f:
-    ranges = [[[int(i) for i in r.split("-")] for r in l.split(",")] for l in f.read().split("\n")]
+    ranges = [[[int(i) for i in range.split("-")] for range in pair.split(",")] for pair in f.read().split("\n")]
 
-countFull = 0
-count = 0
+countFullOverlaps = 0
+countOverlaps = 0
 
 for r in ranges:
     if r[0][0] >= r[1][0] and r[0][1] <= r[1][1] or r[0][0] <= r[1][0] and r[0][1] >= r[1][1]:
-        countFull += 1
-        count += 1
-    elif r[0][1] >= r[1][0] and r[1][1] >= r[0][0]:
-        count += 1
+        countFullOverlaps += 1
+    if r[1][0] <= r[0][1] and r[0][0] <= r[1][1]:
+        countOverlaps += 1
 
-print(f"Part One: {countFull}")
-print(f"Part Two: {count}")
+print(f"Part One: {countFullOverlaps}")
+print(f"Part Two: {countOverlaps}")
