0997427c96154c2c6de04cb42bfc10c64f6b99d8
Jon <stu229763@uni-kiel.de>
2022-12-06 06:23:21 +0100

    day06: Python

--- 2022/06/day06.py
+++ 2022/06/day06.py
@@ -1,18 +1,16 @@
 with open("input.txt") as f:
-    stream= f.read()
+    stream = f.read()
 
-stack = []
+def findMarker(length):
+    stack = []
+    for i, c in enumerate(stream):
+        stack.append(c)
 
-for i, c in enumerate(stream):
+        if len(stack) == length:
+            if len(set(stack)) == length:
+                return(i+1)
+                
+            stack.pop(0)
 
-    stack.append(c)
-
-    if len(stack) == 4:
-        dist = True
-        for j, c1 in enumerate(stack):
-            if c1 in stack[j+1:]:
-                dist = False
-        if dist:
-            print(i+1)
-            break
-        stack.pop(0)
+print(f"Part One: {findMarker(4)}")
+print(f"Part One: {findMarker(14)}")
