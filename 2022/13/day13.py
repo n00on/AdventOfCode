c752fc11e7339199b79407291be2dd1a495bdf24
jonstue <stu229763@mail.uni-kiel.de>
2022-12-13 23:34:46 +0100

    Day13: Cleanup

--- 2022/13/day13.py
+++ 2022/13/day13.py
@@ -5,48 +5,45 @@ def compare(left, right) -> int:
     match left, right:
         case int(left), int(right):
             return left - right
-
         case int(left), list(right):
-            return compare([left], right)
-
+            return compare_list([left], right)
         case list(left), int(right):
-            return compare(left, [right])
-
+            return compare_list(left, [right])
         case list(left), list(right):
-            for j in range(min(len(left), len(right))):
-                c = compare(left[j], right[j])
-                if c != 0:
-                    return c
-            return len(left) - len(right)
+            return compare_list(left, right)
 
-sum = 0
-for i in range(0, len(packets), 2):
-    if compare(packets[i], packets[i+1]) <= 0:
-        sum += (i//2) + 1
+def compare_list(left : list, right : list) -> int:
+    for j in range(min(len(left), len(right))):
+        c = compare(left[j], right[j])
+        if c != 0:
+            return c
+    return len(left) - len(right)
 
-print(f"Part One: {sum}")
+def part_one() -> int:
+    sum = 0
+    for i in range(0, len(packets), 2):
+        if compare(packets[i], packets[i+1]) <= 0:
+            sum += (i//2) + 1
+    return sum
 
 # Short mergesort implementation to not use libraries lol
 def merge_sort(input : list) -> list:
     if len(input) <= 1:
         return input
-    mid = len(input)//2
-    l = merge_sort(input[:mid])
-    r = merge_sort(input[mid:])
-    return merge(l, r)
-
+    mid = len(input) // 2
+    return merge(merge_sort(input[:mid]), merge_sort(input[mid:]))
+    
 def merge(l : list, r : list) -> list:
     result = []
     while len(l) > 0 and len(r) > 0:
-        if compare(l[0], r[0]) <= 0:
-            result.append(l.pop(0))
-        else:
-            result.append(r.pop(0))
-    result += l
-    result += r
-    return result
+        next = l if compare(l[0], r[0]) <= 0 else r
+        result.append(next.pop(0))
+    return result + l + r
 
-divider = [[[2]], [[6]]]
-s = merge_sort(packets + divider)
-decoder_key = (s.index(divider[0]) + 1) * (s.index(divider[1]) + 1)
-print(f"Part Two: {decoder_key}")
+def part_two() -> int:
+    divider = [[[2]], [[6]]]
+    s = merge_sort(packets + divider)
+    return (s.index(divider[0]) + 1) * (s.index(divider[1]) + 1)
+
+print(f"Part One: {part_one()}")
+print(f"Part Two: {part_two()}")
