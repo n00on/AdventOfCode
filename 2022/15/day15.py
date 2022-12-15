151b1862ada9e64667ba8f61224fd1f17a072f41
jonstue <stu229763@mail.uni-kiel.de>
2022-12-15 09:44:33 +0100

    Day15: Slow and not clean

--- 2022/15/day15.py
+++ 2022/15/day15.py
@@ -16,5 +16,30 @@ def calcLine(row = 10):
             taken.remove(b[0])
     return len(taken)
 
+def findHole(row, bound = 20):
+    ranges = []
+    for sensor, beacon in zip(sensors, beacons):
+        diff = abs(sensor[1] - beacon[1]) + abs(sensor[0] - beacon[0])
+        diff2row = diff - abs(sensor[1] - row)
+        se = (max(0, sensor[0] - diff2row), min(sensor[0] + diff2row, bound))
+        if se[0] <= se[1]:
+            ranges.append(se)
 
-print(calcLine(2000000))
+    last = 0
+    for s, e in sorted(ranges):
+        if s <= last + 1:
+            last = max(last, e)
+        else:
+            return (s-1) * bound + row
+    return -1
+
+def part2(bound = 20):
+    tuning_frequency = -1
+    for i in range(bound):
+        tuning_frequency = findHole(i, bound)
+        if tuning_frequency != -1:
+            return tuning_frequency
+
+
+print(f"Part One: {calcLine(2000000)}")
+print(f"Part Two: {part2(4000000)}")
