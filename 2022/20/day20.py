b7799c507b4f7c16cd952933ee073a5d2feb260b
jstu <jon.stuehrwoldt_extern@ppi.de>
2022-12-20 20:09:58 +0100

    Day20: Both

--- 2022/20/day20.py
+++ 2022/20/day20.py
@@ -1,27 +1,37 @@
 with open("input.txt") as f:
-    nums = [int(n) for n in f.read().split("\n")]
+    input = [int(i) for i in f.read().split("\n")]
 
-def part1(numbers : list) -> int:
-    nums = [(n, False) for n in numbers]
-    i = 0
-    while i < len(nums):
-        n, b = nums[i]
-        if b or n == 0:
-            i += 1
+def mix(nums : list[int], order : list[int]):
+    for i, index in enumerate(order):
+        n = nums[index]
+        if n == 0:
             continue
 
-        newpos = i + n
-        newpos += newpos // len(nums)
-        newpos %= len(nums)
+        newpos = index + n
+        newpos %= len(nums) - 1
+        dir = 1 if newpos > index else -1
+        
+        r = range(newpos, index) if dir == -1 else range(index+1,newpos+1)
+        for j in range(len(nums)):
+            if order[j] in r:
+                order[j] = order[j] - dir
+        
+        nums.pop(index)
+        nums.insert(newpos, n)
 
-        nums.pop(i)
-        nums.insert(newpos, (n, True))
+        order[i] = newpos
 
-    index0 = nums.index((0, False))
+def getCoordinates(rounds = 1, factor = 1):
+    order = [i for i in range(len(input))]
+    nums = [n*factor for n in input]
+    for _ in range(rounds):
+        mix(nums, order)
+
+    index0 = nums.index(0)
     sum = 0
     for offset in [1000,2000,3000]:
-        sum += nums[(index0+offset) % len(nums)][0]
-    return sum
+        sum += nums[(index0+offset) % len(nums)]
+    return sum 
 
-
-print(f"Part One: {part1(nums)}")
+print(f"Part One: {getCoordinates()}")
+print(f"Part Two: {getCoordinates(10, 811589153)}")
