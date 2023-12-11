f51e83cf696f59fad2b016a3b2951eb4d396d0cd
Jon <stu229763@uni-kiel.de>
2023-12-11 11:40:27 +0100

    Day11: Cleanup

--- 2023/11/day11.ex
+++ 2023/11/day11.ex
@@ -1,64 +1,57 @@
 defmodule Day11 do
   
   def start() do
-    parsed = File.read!("input.txt") |> String.replace("\r", "") |> String.split() |> Enum.map(&String.graphemes/1)
+    parsed = File.read!("input.txt") |> String.replace("\r", "") |> String.split()
+    |> Enum.map(&String.graphemes/1) 
     IO.puts("Part 1: " <> inspect(part_1(parsed)))
     IO.puts("Part 2: " <> inspect(part_2(parsed)))
   end
 
-  def part_1(map) do
-    find_all_distances(map)
+  def part_1(image) do
+    find_all_distances(image)
   end
 
-  def find_empty_columns(map) do
-    for x <- 0..(length(Enum.at(map, 0))-1), reduce: [] do
-      acc -> 
-        if (for y <- 0..(length(map)-1), reduce: true do
-          acc -> acc and at(map, x, y) != "#"
-        end), do: [x | acc], else: acc
+  def part_2(image) do
+    find_all_distances(image, 1_000_000)
+  end
+
+  def find_empty_columns(image) do
+    for x <- 0..(length(Enum.at(image, 0))-1), reduce: [] do
+      list -> if Enum.all?(image, fn row -> Enum.at(row, x) != "#" end), do: [x | list], else: list
     end
   end
 
-  def find_empty_rows(map) do
-    for y <- 0..(length(map)-1), reduce: [] do
-      acc -> 
-        if (for x <- 0..(length(Enum.at(map, y))-1), reduce: true do
-          acc -> acc and at(map, x, y) != "#"
-        end), do: [y | acc], else: acc
+  def find_empty_rows(image) do
+    for {row, y} <- Stream.with_index(image), reduce: [] do
+      list -> if Enum.all?(row, fn pixel -> pixel != "#" end), do: [y | list], else: list
     end
   end
 
-  def find_galaxies(map) do
-    for y <- 0..(length(map)-1), reduce: [] do
-      acc -> 
-        (for x <- 0..(length(Enum.at(map, y))-1), reduce: [] do
-          acc -> if at(map, x, y) == "#", do: [{x, y} | acc], else: acc
-        end) ++ acc
+  def find_galaxies(image) do
+    for {row, y} <- Stream.with_index(image), reduce: [] do
+      list -> 
+        (for {pixel, x} <- Stream.with_index(row), reduce: [] do
+          acc -> if pixel == "#", do: [{x, y} | acc], else: acc
+        end) ++ list
     end
   end
 
-  def find_all_distances(map, expansion \\ 1) do
-    emptyr = find_empty_rows(map)
-    emptyc = find_empty_columns(map)
-    galaxies = find_galaxies(map)
-    for {{x1, y1}, i} <- Enum.with_index(galaxies), reduce: 0 do
-      acc -> acc + 
-        for {x2, y2} <- Enum.slice(galaxies, (i+1)..-1//1), reduce: 0 do
-          acc -> acc +
-            abs(x2 - x1) + abs(y2 - y1) +
-            expansion * (
-              length(Enum.filter(emptyc, fn x -> x > x2 and x < x1 or x > x1 and x < x2 end)) + 
-              length(Enum.filter(emptyr, fn y -> y > y2 and y < y1 or y > y1 and y < y2 end)))
-        end
-      end
+  def find_all_distances(image, expansion \\ 2) do
+    empty_rows = find_empty_rows(image)
+    empty_cols = find_empty_columns(image)
+    galaxies = find_galaxies(image)
+    for {{x1, y1}, i} <- Stream.with_index(galaxies), 
+        {{x2, y2}, j} <- Stream.with_index(galaxies), j > i, reduce: 0 do
+      sum -> sum +
+        abs(x2 - x1) + abs(y2 - y1) +
+        (expansion-1) * (in_range(empty_rows, y1, y2) + in_range(empty_cols, x1, x2))
+    end
   end
 
-  def part_2(map) do
-    find_all_distances(map, 999999)
-  end
-
-  def at(map, x, y) do
-    map |> Enum.at(y) |> Enum.at(x)
+  # how many in list are between l and r
+  def in_range(list, r, l) when r > l, do: in_range(list, l, r)
+  def in_range(list, l, r) do
+    list |> Enum.filter(fn val -> l < val and val < r end) |> length()
   end
 
 end
