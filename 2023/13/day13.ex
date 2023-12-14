571077adc128e061f5a09b302df780e37551e268
Jon <jonstuehrwoldt@gmx.de>
2023-12-14 17:50:10 +0100

    Day13: A

--- 2023/13/day13.ex
+++ 2023/13/day13.ex
@@ -1,5 +1,5 @@
 defmodule Day13 do
-  
+
   def start() do
     input = File.read!("input.txt") |> String.replace("\r", "") |> String.split("\n\n")
     |> Enum.map(fn pattern -> String.split(pattern) |> Enum.map(&String.graphemes/1) end)
@@ -8,21 +8,28 @@ defmodule Day13 do
   end
 
   def part_1(input) do
+    (input |> Stream.map(&col_mirror/1)
+    |> Enum.sum) + 100 *
     (input |> Stream.map(&row_mirror/1)
-    #|> Enum.sum
-    ) |> Enum.to_list
-    #+ 100 *
+    |> Enum.sum)
   end
 
   def row_mirror(pattern) do
     height = length(pattern)
     rows = (for y <- 1..(height - 1) do
-      range = min(height - y, y)
-      IO.inspect((y-range)..(y - 1))
-      Enum.all?((y-range)..(y - 1), fn i -> Enum.at(pattern, y-i - 1) == Enum.at(pattern, y+i) end)
+      range = min(height - y, y) - 1
+      #IO.inspect({0..range, (y-range-1)..(y+range)})
+      Enum.all?(0..range, fn i -> Enum.at(pattern, y-i - 1) == Enum.at(pattern, y+i) end)
     end
-    |> Enum.find_index(fn b -> b end)) 
-    if rows, do: rows, else: 0
+    |> Enum.find_index(fn b -> b end))
+    if rows, do: rows + 1, else: 0
+  end
+
+  def col_mirror(pattern) do
+    pattern
+    |> Stream.zip()
+    |> Enum.map(&Tuple.to_list/1)
+    |> row_mirror()
   end
 
   def part_2(input) do
@@ -32,4 +39,3 @@ defmodule Day13 do
 end
 
 Day13.start()
-
