a6a1981765673c9e15a1fbcc638605d9fdc56fdf
Jon <jonstuehrwoldt@gmx.de>
2023-12-14 14:37:22 +0100

    Day14: A with tuples

--- 2023/14/day14.ex
+++ 2023/14/day14.ex
@@ -2,23 +2,27 @@ defmodule Day14 do
 
   def start() do
     input = File.read!("input.txt") |> String.replace("\r", "") |> String.split()
-    |> Enum.map(&String.graphemes/1)
+    |> Enum.map(&String.graphemes/1) |> Enum.map(&List.to_tuple/1) |> List.to_tuple()
     IO.inspect(part_1(input), label: "Part 1: ")
     IO.inspect(part_2(input), label: "Part 2: ")
   end
 
   def part_1(input) do
+    map = for _ <- 0..4000 do
+      roll_north(input, 1)
+    end
     map = roll_north(input, 1)
-    for {row, y} <- Stream.with_index(map), reduce: 0 do
-      sum -> sum + (length(map) - y) * Enum.count(row, fn cell -> cell == "O" end)
+    for y <- 0..(tuple_size(map) - 1), reduce: 0 do
+      sum ->
+        sum + (tuple_size(map) - y) * Enum.count(elem(map, y) |> Tuple.to_list, fn cell -> cell == "O" end)
     end
   end
 
-  def roll_north(map, y) when length(map) == y, do: map
+  def roll_north(map, y) when tuple_size(map) == y, do: map
   def roll_north(map, y) do
     #IO.inspect(map)
-    row = Enum.at(map, y)
-    for x <- 0..(length(row)-1), reduce: map do
+    row = elem(map, y)
+    for x <- 0..(tuple_size(row)-1), reduce: map do
       acc ->
         if at(acc, x, y) == "O" do
           yn = roll_cell(acc, y, x)
@@ -44,14 +48,13 @@ defmodule Day14 do
   end
 
   def at(map, x, y) do
-    map |> Enum.at(y) |> Enum.at(x)
+    map |> elem(y) |> elem(x)
   end
 
   def update(map, x, y, xn, yn) do
     #IO.inspect({map, x, y, xn, yn})
-    map |> List.replace_at(y, List.replace_at(Enum.at(map, y), x, "."))
-    |> List.replace_at(yn,
-    List.replace_at(Enum.at(map, yn), xn, "O"))
+    map |> put_elem(y, put_elem(elem(map, y), x, "."))
+    |> put_elem(yn, put_elem(elem(map, yn), xn, "O"))
   end
 
 end
