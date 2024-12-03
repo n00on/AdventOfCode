f631289eb19a4f015b4e690aa43941dae95a4eeb
Jon <jonstuehrwoldt@gmx.de>
2024-12-03 09:57:13 +0100

    2024 Day03: B (Haskell)

--- 2024/03/day03.hs
+++ 2024/03/day03.hs
@@ -1,9 +1,9 @@
-import Text.Parsec (parse)
 import Text.Read (readMaybe)
 
 main = do
     input <- readFile "input.txt"
     print $ part1 input
+    print $ part2 True input
 
 part1 :: String -> Int
 part1 ('m':'u':'l':'(': rest) = case parseRest rest of
@@ -12,13 +12,22 @@ part1 ('m':'u':'l':'(': rest) = case parseRest rest of
 part1 (_:rest) = part1 rest
 part1 [] = 0
 
+part2 :: Bool -> String -> Int
+part2 b ('d':'o':'n':'\'':'t':'(':')': rest) = part2 False rest
+part2 b ('d':'o':'(':')': rest) = part2 True rest
+part2 False (_:rest) = part2 False rest
+part2 b ('m':'u':'l':'(': rest) = case parseRest rest of
+                                    Just (n, unparsed) -> n + part2 b unparsed
+                                    Nothing            -> part2 b rest
+part2 b (_:rest) = part2 b rest
+part2 b [] = 0
+
 
 parseRest :: String -> Maybe (Int, String)
-parseRest s = case parseInt 3 s of
-                    Just (x1,',':s1) -> case parseInt 3 s1 of
-                        Just (x2,')':s2) ->  Just (x1*x2, s2)
-                        _ -> Nothing
-                    _ -> Nothing
+parseRest s = do 
+    (x1,',':s1) <- parseInt 3 s
+    (x2,')':s2) <- parseInt 3 s1
+    return (x1*x2, s2)
 
 
 parseInt :: Int -> String -> Maybe (Int, String)
