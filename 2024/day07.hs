b4f2726464b24c617a64942bf7be220e64f94007
Jon <jonstuehrwoldt@gmx.de>
2024-12-07 11:51:55 +0100

    2024: Day07 (Haskell)

---
+++ 2024/day07.hs
@@ -0,0 +1,26 @@
+
+main = do
+    eqs <- map (readEQ . words) . lines <$> readFile "input/07.txt"
+    print $ part1 eqs
+    print $ part2 eqs
+
+readEQ :: [String] -> (Int, [Int])
+readEQ (r:os) = (read (take (length r - 1) r), map read os)
+
+part1 :: [(Int, [Int])] -> Int
+part1 = sum . map fst . filter (checkEQ [(*), (+)])
+
+checkEQ :: [Int -> Int -> Int] -> (Int, [Int]) -> Bool
+checkEQ ops (r, o:os) = checkEQ' o os
+    where
+        checkEQ' :: Int -> [Int] -> Bool
+        checkEQ' n []     = n == r
+        checkEQ' n (o:os) = n <  r && any (\op -> checkEQ' (op n o) os) ops
+
+
+part2 :: [(Int, [Int])] -> Int
+part2 = sum . map fst . filter (checkEQ [(*), (+), concat])
+    where
+        concat :: Int -> Int -> Int
+        concat x y =  x * (10 ^ (1 + (floor . logBase 10 . fromIntegral) y)) + y
+        --concat x y = read $ show x ++ show y
